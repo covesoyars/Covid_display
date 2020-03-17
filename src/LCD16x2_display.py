91601b142482b41f73629b72c18bc941a9603414
Cove Soyars <soyarsc@vcu.edu>
2020-03-17 09:33:53 -0400

    lcd initial test

--- src/LCD16x2_display.py
+++ src/LCD16x2_display.py
@@ -1,4 +1,24 @@
 from CovidDisplayScraper import CovidDisplay
 
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd as characterlcd
+
 """Displays COVID-19 stats on LCD 16x2 screen"""
+
+lcd_columns = 16
+lcd_rows = 2
+
+# compatible with all versions of RPI as of Jan. 2019
+# v1 - v3B+
+lcd_rs = digitalio.DigitalInOut(board.D22)
+lcd_en = digitalio.DigitalInOut(board.D17)
+lcd_d4 = digitalio.DigitalInOut(board.D25)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d6 = digitalio.DigitalInOut(board.D23)
+lcd_d7 = digitalio.DigitalInOut(board.D18)
+
+# Initialise the lcd class
+lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
+									  lcd_d7, lcd_columns, lcd_rows)
 
